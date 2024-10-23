3145b20e4ce0e1a3e7f881b1dbb3d6b3d1b4c301
audionai <euxddesigner@gmail.com>
2024-10-23 05:03:09 -0700

    Update page.tsx

--- app/myvoice/page.tsx
+++ app/myvoice/page.tsx
@@ -15,7 +15,7 @@ const Myvoice = () => {
             <button onClick={handleClick} className="go_back"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 8L6 12M6 12L10 16M6 12L18 12" stroke="rgb(var(--black))" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>
             </button>
             <p className={styles.train_voice}>Train Your Voice</p>
-            <label for="model" className={styles.label}>
+            <label className={styles.label}>
                 <input type="file" id="model" className={styles.input_voice} accept="audio/*,audio/mp3,audio/wav,audio/aac"/>
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 18C10 19.1046 10.8954 20 12 20C13.1046 20 14 19.1046 14 18V14H18C19.1046 14 20 13.1046 20 12C20 10.8954 19.1046 10 18 10H14V6C14 4.89543 13.1046 4 12 4C10.8954 4 10 4.89543 10 6V10L6 10C4.89543 10 4 10.8954 4 12C4 13.1046 4.89543 14 6 14L10 14V18Z" fill="rgb(var(--color))"/></svg>
 
